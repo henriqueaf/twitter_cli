f50e56c41909409e625c5ec6627db63c3a4682fd
Henrique Almeida <henrique.af1@gmail.com>
2019-04-23 11:51:40 -0300

    Add oauther lib

--- mix.exs
+++ mix.exs
@@ -24,7 +24,8 @@ defmodule TwitterCli.MixProject do
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
       {:httpoison, "~> 1.4"},
-      {:poison, "~> 3.1.0"}
+      {:poison, "~> 3.1.0"},
+      {:oauther, "~> 1.1"}
     ]
   end
 end
